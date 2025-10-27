5f93b4c24798d6a97287b0c83d2a47060bc0ea5f
mireutale <mireutale@gmail.com>
2025-10-27 22:59:02 +0900

    feat: create main

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -1,7 +1,25 @@
 package racingcar;
 
+import racingcar.validation.AttemptCountValidator;
+import racingcar.validation.CarNameValidator;
+import racingcar.view.InputView;
+import racingcar.view.OutputView;
+
+import java.util.List;
+
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        List<String> names = InputView.readCarNames();
+        CarNameValidator.validate(names);
+
+        int attemptCount = AttemptCountValidator.parseAndValidate(InputView.readAttemptCount());
+
+        RacingGame game = new RacingGame(names);
+        OutputView.printExecutionHeader();
+        for (int i = 0; i < attemptCount; i++) {
+            game.runOneRound();
+            OutputView.printRoundStatus(game.getCars());
+        }
+        OutputView.printWinners(game.findWinnerNames());
     }
 }
